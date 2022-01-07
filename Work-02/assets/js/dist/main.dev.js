24b22f774dd443c53ecbcbe517e09d10f63098c4
Dima <dim84848484@gmail.com>
2022-01-07 13:55:00 +0200

    update files and links(add Parallax_Flipping_Cards)

--- Work-02/assets/js/dist/main.dev.js
+++ Work-02/assets/js/dist/main.dev.js
@@ -77,6 +77,10 @@ $(function () {
     // $(".hamburger").toggleClass("is-active");
     $(".mobile_menu_wrap .hamburger").toggleClass("is-active");
     $("body").toggleClass("open");
+  }); // Закрытие меню бургер при нажатии на пунты меню
+
+  $(".sidemenu ul li a").on('click', function () {
+    $("body").removeClass("open");
   }); // Map Leaflet
   // инициализируем карту по клику
 
