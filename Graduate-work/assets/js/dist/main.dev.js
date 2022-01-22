980673721da240c4b3ec1a64eca60f4634985a64
Dima <dim84848484@gmail.com>
2022-01-22 02:04:51 +0200

    update graduate works

--- Graduate-work/assets/js/dist/main.dev.js
+++ Graduate-work/assets/js/dist/main.dev.js
@@ -1,1 +1,34 @@
 "use strict";
+
+$(function () {
+  // Fix Header Scroll
+  // $(window).on('scroll', function(){
+  // if($(window).scrollTop()>$(".main").height()){
+  // вариант ниже с высотой а выше с классом
+  //         if($(window).scrollTop()>0){
+  //             if(!$("header").hasClass("fixed_header")) {
+  //                 $("header").addClass("fixed_header");
+  //             }
+  //     }else{
+  //             if($("header").hasClass("fixed_header")){
+  //                 $("header").removeClass("fixed_header");
+  //             }
+  //         }
+  // });
+  // Плавный скролл по меню!
+  // $("#main_menu a").on('click', function(e){
+  //     e.preventDefault();
+  //     const top = $($(this).attr("href")).offset().top-60;
+  //     $("html, body").animate({scrollTop:top+'px'}, 900);
+  // });
+  // Hamburger-menu
+  $(".hamburger, .page_overlay").on('click', function () {
+    // $(".hamburger").toggleClass("is-active");
+    $(".mobile_menu_wrap .hamburger").toggleClass("is-active");
+    $("body").toggleClass("open");
+  }); // Закрытие меню бургер при нажатии на пунты меню
+
+  $(".sidemenu ul li a").on('click', function () {
+    $("body").removeClass("open");
+  });
+});
