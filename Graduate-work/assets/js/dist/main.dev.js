61ebe7578eec498d469bb1294526b1f2ad8119f0
Dima <dim84848484@gmail.com>
2022-02-07 01:57:58 +0200

    update graduate work

--- Graduate-work/assets/js/dist/main.dev.js
+++ Graduate-work/assets/js/dist/main.dev.js
@@ -43,71 +43,61 @@ $(function () {
     $("body").removeClass("open");
   }); // Модальное окно order tour
 
-  $.when(getCard()).then(function () {
-    // открыть по кнопке
-    $('#booking_btn, #card_btn').click(function () {
-      $('.booking__modal').fadeIn();
-      $('.booking__modal').addClass('disabled');
-    }); // закрыть на крестик
+  getCard(); // закрыть по клику вне окна
 
-    $('.modal__close_btn').click(function () {
-      $('.booking__modal').fadeOut(600); // закрытие с плавной анимацией, где 600 это время в мс
-    }); // закрыть по клику вне окна
+  $(document).mouseup(function (e) {
+    var popup = $('.modal__content');
 
-    $(document).mouseup(function (e) {
-      var popup = $('.modal__content');
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
+      $('.booking__modal').fadeOut(600);
+    }
+  }); // закрыть по ESC
 
-      if (e.target != popup[0] && popup.has(e.target).length === 0) {
-        $('.booking__modal').fadeOut(600);
-      }
-    }); // закрыть по ESC
+  $(document).on('keydown', function (event) {
+    if (event.keyCode == 27) {
+      $('.booking__modal').fadeOut(600);
+    }
+  }); // Маска номера телефона
 
-    $(document).on('keydown', function (event) {
-      if (event.keyCode == 27) {
-        $('.booking__modal').fadeOut(600);
-      }
-    }); // Маска номера телефона
+  $(function () {
+    $('#booking_phone').mask('+38 (099) 999-99-9?9');
+  }); // Валидация + отправка формы на Telegram BOT
 
-    $(function () {
-      $('#booking_phone').mask('+38 (099) 999-99-9?9');
-    }); // Валидация + отправка формы на Telegram BOT
+  $("#my_booking-form").on('submit', function (e) {
+    e.preventDefault();
+    var nameInput = document.getElementById('booking_name');
+    var surnameInput = document.getElementById('booking_surname');
+    var emailInput = document.getElementById('booking_email');
+    var phoneInput = document.getElementById('booking_phone');
+    var tourSelect = document.getElementById('choice_tour');
+    var BOT_TOKEN = '5019836353:AAEY0Hztn5q-UaklaKWXMoDqbUyn0MhEzhc'; //  @get_id_bot and /get_id
 
-    $("#my_booking-form").on('submit', function (e) {
-      e.preventDefault();
-      var nameInput = document.getElementById('booking_name');
-      var surnameInput = document.getElementById('booking_surname');
-      var emailInput = document.getElementById('booking_email');
-      var phoneInput = document.getElementById('booking_phone');
-      var tourSelect = document.getElementById('choice_tour');
-      var BOT_TOKEN = '5019836353:AAEY0Hztn5q-UaklaKWXMoDqbUyn0MhEzhc'; //  @get_id_bot and /get_id
+    var CHAT_ID = '704440668'; //  let text = encodeURI("<b>Email:</b> "+$("#exampleInputEmail1").val()+"\n<b>Subject:</b> "+$("#exampleInputPassword1").val()+"\n<b>Message:</b> "+$("#massage").val());
 
-      var CHAT_ID = '704440668'; //  let text = encodeURI("<b>Email:</b> "+$("#exampleInputEmail1").val()+"\n<b>Subject:</b> "+$("#exampleInputPassword1").val()+"\n<b>Message:</b> "+$("#massage").val());
+    var text = encodeURI("Name: ".concat(nameInput.value, ", Surname:").concat(surnameInput.value, ", \n            Email: ").concat(emailInput.value, ", Phone: ").concat(phoneInput.value, ", Tour: ").concat(tourSelect.value));
 
-      var text = encodeURI("Name: ".concat(nameInput.value, ", Surname:").concat(surnameInput.value, ", \n                Email: ").concat(emailInput.value, ", Phone: ").concat(phoneInput.value, ", Tour: ").concat(tourSelect.value));
-
-      if (nameInput.value !== '' && surnameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '' && tourSelect.value !== '') {
-        $.get("https://api.telegram.org/bot".concat(BOT_TOKEN, "/sendMessage?chat_id=").concat(CHAT_ID, "&text=") + text + '&parse_mode=html', function (json) {
-          if (json.ok) {
-            $("#my_booking-form").trigger('reset');
-            Swal.fire({
-              position: 'top-end',
-              icon: 'success',
-              title: 'Your message send!',
-              showConfirmButton: false,
-              timer: 3000
-            });
-          }
-        });
-      } else {
-        Swal.fire({
-          position: 'top-end',
-          icon: 'warning',
-          title: 'Fill all field!',
-          showConfirmButton: false,
-          timer: 3000
-        });
-      }
-    });
+    if (nameInput.value !== '' && surnameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '' && tourSelect.value !== '') {
+      $.get("https://api.telegram.org/bot".concat(BOT_TOKEN, "/sendMessage?chat_id=").concat(CHAT_ID, "&text=") + text + '&parse_mode=html', function (json) {
+        if (json.ok) {
+          $("#my_booking-form").trigger('reset');
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your message send!',
+            showConfirmButton: false,
+            timer: 3000
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'Fill all field!',
+        showConfirmButton: false,
+        timer: 3000
+      });
+    }
   }); // Slick-slider Place
   // getCard();
 
@@ -197,50 +187,64 @@ $(function () {
 }); // Динамические карты блока place
 
 function getCard() {
-  $.ajax({
-    url: 'common/card.json',
-    type: 'get',
-    dataType: 'json',
-    success: function success(json) {
-      var html = '';
-      json.forEach(function (card) {
-        html += "\n                    <li class=\"card__item card-first\">\n                        <div class=\"card__image\" id=\"card-img\">\n                            <a class=\"card__image_link colorbox\" data-fancybox=\"group-1\" href=\"assets/images/place_image/place_1-min(b).jpg\">\n                                <img class=\"card__pic\"\n                                    src=\"assets/images/place_image/".concat(card.pic.image, "\" alt=\"place_image\">\n                                <div class=\"card__price\">").concat(card.pic.price, "</div>\n                            </a>\n                        </div>\n                        <div class=\"card__content\">\n                            <div class=\"card__title\">\n                                <h6>").concat(card.title, "</h6>\n                            </div>\n                            <div class=\"card__subtitle subtitle\">\n                                <p>").concat(card.description, "</p>\n                            </div>\n                            <div class=\"card__link\">\n                                <button type=\"button\" class=\"card__link_text text_orange\" id=\"card_btn\">\n                                ").concat(card.link, "</button>\n                            </div>\n                        </div>\n                    </li>\n                ");
-      });
-      $("#page_card").append(html);
-      $("#card_tour").slick('slickAdd', html);
-    },
-    error: function error() {
-      // modal window sweet-aler2
-      Swal.fire({
-        position: 'top-end',
-        icon: 'error',
-        title: 'Oops...',
-        text: "The tour-cards don't load!",
-        showConfirmButton: false,
-        timer: 4000
-      });
+  return regeneratorRuntime.async(function getCard$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap($.ajax({
+            url: 'common/card.json',
+            type: 'get',
+            dataType: 'json',
+            success: function success(json) {
+              var html = '';
+              json.forEach(function (card) {
+                html += "\n                    <li class=\"card__item card-first\">\n                        <div class=\"card__image\" id=\"card-img\">\n                            <a class=\"card__image_link colorbox\" data-fancybox=\"group-1\" href=\"assets/images/place_image/".concat(card.pic.big_image, "\" title=\"").concat(card.title, "\">\n                                <img class=\"card__pic\"\n                                    src=\"assets/images/place_image/").concat(card.pic.image, "\" alt=\"place_image\">\n                                <div class=\"card__price\">").concat(card.pic.price, "</div>\n                            </a>\n                        </div>\n                        <div class=\"card__content\">\n                            <div class=\"card__title\">\n                                <h6>").concat(card.title, "</h6>\n                            </div>\n                            <div class=\"card__subtitle subtitle\">\n                                <p>").concat(card.description, "</p>\n                            </div>\n                            <div class=\"card__link\">\n                                <button type=\"button\" class=\"card__link_text text_orange\" id=\"card_btn\">\n                                ").concat(card.link, "</button>\n                            </div>\n                        </div>\n                    </li>\n                ");
+              });
+              $("#page_card").append(html);
+              $("#card_tour").slick('slickAdd', html);
+            },
+            error: function error() {
+              // modal window sweet-aler2
+              Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Oops...',
+                text: "The tour-cards don't load!",
+                showConfirmButton: false,
+                timer: 4000
+              });
+            }
+          }));
+
+        case 2:
+          // открыть по кнопке
+          $('#booking_btn, #card_btn').click(function () {
+            $('.booking__modal').fadeIn();
+            $('.booking__modal').addClass('disabled');
+          }); // закрыть на крестик
+
+          $('.modal__close_btn').click(function () {
+            $('.booking__modal').fadeOut(600); // закрытие с плавной анимацией, где 600 это время в мс
+          }); // colorbox plugin
+
+          $(function () {
+            $("a.card__image_link").colorbox({
+              maxWidth: "90%",
+              maxHeight: "90%",
+              closeButton: "true"
+            });
+          });
+
+        case 5:
+        case "end":
+          return _context.stop();
+      }
     }
-  });
-  $(function () {
-    $("a.colorbox").colorbox({
-      maxWidth: "90%",
-      maxHeight: "90%"
-    });
   });
 }
 
-; // Подключение lightGallery
-
-lightGallery(document.querySelector('.gallery__album', '.album__page'), {
-  plugins: [lgZoom, lgThumbnail],
-  thumbnail: true,
-  zoom: true,
-  actualSize: true,
-  animateThumb: true,
-  zoomFromOrigin: true,
-  speed: 500,
-  licenseKey: 'your_license_key'
-}); // Динамические карты блока clients
+; // Динамические карты блока clients
 
 function getReview() {
   $.ajax({
@@ -250,7 +254,7 @@ function getReview() {
     success: function success(json) {
       var html = '';
       json.forEach(function (item) {
-        html += "\n                <li class=\"clients__cards\">\n                    <div class=\"clients__content\">\n                        <div class=\"clients__review subtitle\">\n                            <p>".concat(item.review, "\n                            </p>\n                        </div>\n                        <div class=\"clients__item\">\n                            <div class=\"clients__footer\">\n                                <div class=\"clients__avatar\">\n                                    <img class=\"clients__avatar_photo lazy\"\n                                        src=\"assets/images/clients/").concat(item.author.avatar, "\" alt=\"author-pic\">\n                                </div>\n                                <div class=\"clients__info\">\n                                    <div class=\"clients__info_author\">").concat(item.author.name, "</div>\n                                    <div class=\"clients__info_occupation\">").concat(item.author.occupation, "</div>\n                                </div>\n                            </div>\n                        </div>\n                    </div>\n                </li>\n                ");
+        html += "\n                <li class=\"clients__cards\">\n                    <div class=\"clients__content\">\n                        <div class=\"clients__review subtitle\">\n                            <p>".concat(item.review, "\n                            </p>\n                        </div>\n                        <div class=\"clients__item\">\n                            <div class=\"clients__footer\">\n                                <div class=\"clients__avatar\">\n                                    <img class=\"clients__avatar_photo\"\n                                        src=\"assets/images/clients/").concat(item.author.avatar, "\" alt=\"author-pic\">\n                                </div>\n                                <div class=\"clients__info\">\n                                    <div class=\"clients__info_author\">").concat(item.author.name, "</div>\n                                    <div class=\"clients__info_occupation\">").concat(item.author.occupation, "</div>\n                                </div>\n                            </div>\n                        </div>\n                    </div>\n                </li>\n                ");
       });
       $("#review_clients").append(html);
     },
@@ -269,4 +273,15 @@ function getReview() {
 }
 
 ;
-getReview();
+getReview(); // Подключение lightGallery
+
+lightGallery(document.querySelector('.gallery__album', '.album__page'), {
+  plugins: [lgZoom, lgThumbnail],
+  thumbnail: true,
+  zoom: true,
+  actualSize: true,
+  animateThumb: true,
+  zoomFromOrigin: true,
+  speed: 500,
+  licenseKey: 'your_license_key'
+});
--- Graduate-work/assets/js/main.js
+++ Graduate-work/assets/js/main.js
@@ -34,74 +34,66 @@ $(function(){
     $(".sidemenu ul li a, .mobile__btn").on('click', function () {
         $("body").removeClass("open");
     });
+
     
     // Модальное окно order tour
-    $.when(getCard()).then(function() {
-        // открыть по кнопке
-        $('#booking_btn, #card_btn').click(function() { 
-            $('.booking__modal').fadeIn();
-            $('.booking__modal').addClass('disabled');
-        });
-        // закрыть на крестик
-        $('.modal__close_btn').click(function() { 
-            $('.booking__modal').fadeOut(600); // закрытие с плавной анимацией, где 600 это время в мс
-        });
-        // закрыть по клику вне окна
-        $(document).mouseup(function (e) { 
-            let popup = $('.modal__content');
-            if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-                $('.booking__modal').fadeOut(600);
-            }
-        });
-        // закрыть по ESC
-        $(document).on('keydown',function(event) {
-            if (event.keyCode == 27) {
-                $('.booking__modal').fadeOut(600);
-            }
-        });
-        // Маска номера телефона
-        $(function () {
-            $('#booking_phone').mask('+38 (099) 999-99-9?9');
-        })
-        // Валидация + отправка формы на Telegram BOT
-        $("#my_booking-form").on('submit', function(e){
-            e.preventDefault();
-            let nameInput = document.getElementById('booking_name');
-            let surnameInput = document.getElementById('booking_surname');
-            let emailInput = document.getElementById('booking_email');
-            let phoneInput = document.getElementById('booking_phone'); 
-            let tourSelect = document.getElementById('choice_tour');              
-            const BOT_TOKEN = '5019836353:AAEY0Hztn5q-UaklaKWXMoDqbUyn0MhEzhc';
-        //  @get_id_bot and /get_id
-            const CHAT_ID = '704440668';
-        //  let text = encodeURI("<b>Email:</b> "+$("#exampleInputEmail1").val()+"\n<b>Subject:</b> "+$("#exampleInputPassword1").val()+"\n<b>Message:</b> "+$("#massage").val());
-            let text = encodeURI(`Name: ${nameInput.value}, Surname:${surnameInput.value}, 
-                Email: ${emailInput.value}, Phone: ${phoneInput.value}, Tour: ${tourSelect.value}`);
-            if(nameInput.value !== '' && surnameInput.value !== '' && emailInput.value !== '' 
-            && phoneInput.value !== '' && tourSelect.value !== ''){
-                $.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=`+text+'&parse_mode=html', (json)=>{
-                    if(json.ok){
-                        $("#my_booking-form").trigger('reset');
-                        Swal.fire({
-                            position: 'top-end',
-                            icon: 'success',
-                            title: 'Your message send!',
-                            showConfirmButton: false,
-                            timer: 3000,
-                        })
-                    }
-                });
-            }else{
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'warning',
-                    title: 'Fill all field!',
-                    showConfirmButton: false,
-                    timer: 3000
-                })
-            }
-        });
+    getCard();
+    // закрыть по клику вне окна
+    $(document).mouseup(function (e) { 
+        let popup = $('.modal__content');
+        if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+            $('.booking__modal').fadeOut(600);
+        }
     });
+    // закрыть по ESC
+    $(document).on('keydown',function(event) {
+        if (event.keyCode == 27) {
+            $('.booking__modal').fadeOut(600);
+        }
+    });
+    // Маска номера телефона
+    $(function () {
+        $('#booking_phone').mask('+38 (099) 999-99-9?9');
+    })
+    // Валидация + отправка формы на Telegram BOT
+    $("#my_booking-form").on('submit', function(e){
+        e.preventDefault();
+        let nameInput = document.getElementById('booking_name');
+        let surnameInput = document.getElementById('booking_surname');
+        let emailInput = document.getElementById('booking_email');
+        let phoneInput = document.getElementById('booking_phone'); 
+        let tourSelect = document.getElementById('choice_tour');              
+        const BOT_TOKEN = '5019836353:AAEY0Hztn5q-UaklaKWXMoDqbUyn0MhEzhc';
+    //  @get_id_bot and /get_id
+        const CHAT_ID = '704440668';
+    //  let text = encodeURI("<b>Email:</b> "+$("#exampleInputEmail1").val()+"\n<b>Subject:</b> "+$("#exampleInputPassword1").val()+"\n<b>Message:</b> "+$("#massage").val());
+        let text = encodeURI(`Name: ${nameInput.value}, Surname:${surnameInput.value}, 
+            Email: ${emailInput.value}, Phone: ${phoneInput.value}, Tour: ${tourSelect.value}`);
+        if(nameInput.value !== '' && surnameInput.value !== '' && emailInput.value !== '' 
+        && phoneInput.value !== '' && tourSelect.value !== ''){
+            $.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=`+text+'&parse_mode=html', (json)=>{
+                if(json.ok){
+                    $("#my_booking-form").trigger('reset');
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your message send!',
+                        showConfirmButton: false,
+                        timer: 3000,
+                    })
+                }
+            });
+        }else{
+            Swal.fire({
+                position: 'top-end',
+                icon: 'warning',
+                title: 'Fill all field!',
+                showConfirmButton: false,
+                timer: 3000
+            })
+        }
+    });
+
 
     // Slick-slider Place
     // getCard();
@@ -200,8 +192,8 @@ $(function(){
 });
 
 // Динамические карты блока place
-function getCard(){
-    $.ajax({
+async function getCard(){
+    await $.ajax({
         url:'common/card.json',
         type:'get',
         dataType:'json',
@@ -211,7 +203,7 @@ function getCard(){
                 html += `
                     <li class="card__item card-first">
                         <div class="card__image" id="card-img">
-                            <a class="card__image_link colorbox" data-fancybox="group-1" href="assets/images/place_image/place_1-min(b).jpg">
+                            <a class="card__image_link colorbox" data-fancybox="group-1" href="assets/images/place_image/${card.pic.big_image}" title="${card.title}">
                                 <img class="card__pic"
                                     src="assets/images/place_image/${card.pic.image}" alt="place_image">
                                 <div class="card__price">${card.pic.price}</div>
@@ -235,7 +227,6 @@ function getCard(){
 
             $("#page_card").append(html);
             $("#card_tour").slick('slickAdd', html);
-            
         },
         error:function(){
             // modal window sweet-aler2
@@ -249,28 +240,26 @@ function getCard(){
             })
         }
     });  
+    // открыть по кнопке
+    $('#booking_btn, #card_btn').click(function() { 
+        $('.booking__modal').fadeIn();
+        $('.booking__modal').addClass('disabled');
+    });
+    // закрыть на крестик
+    $('.modal__close_btn').click(function() { 
+        $('.booking__modal').fadeOut(600); // закрытие с плавной анимацией, где 600 это время в мс
+    });
 
+
+    // colorbox plugin
     $(function(){
-        $("a.colorbox").colorbox({
+        $("a.card__image_link").colorbox({
             maxWidth:"90%",
-            maxHeight:"90%"
+            maxHeight:"90%",
+            closeButton:"true"
         });
     });
 };
-
-
-// Подключение lightGallery
-lightGallery(document.querySelector('.gallery__album', '.album__page'), {
-    plugins:[lgZoom, lgThumbnail],
-    thumbnail: true,
-    zoom: true,
-    actualSize: true,
-    animateThumb: true,
-    zoomFromOrigin: true,
-    speed: 500,
-    licenseKey: 'your_license_key',
-});
-
 
 
 // Динамические карты блока clients
@@ -292,7 +281,7 @@ function getReview(){
                         <div class="clients__item">
                             <div class="clients__footer">
                                 <div class="clients__avatar">
-                                    <img class="clients__avatar_photo lazy"
+                                    <img class="clients__avatar_photo"
                                         src="assets/images/clients/${item.author.avatar}" alt="author-pic">
                                 </div>
                                 <div class="clients__info">
@@ -321,3 +310,16 @@ function getReview(){
     });  
 };
 getReview();
+
+
+// Подключение lightGallery
+lightGallery(document.querySelector('.gallery__album', '.album__page'), {
+    plugins:[lgZoom, lgThumbnail],
+    thumbnail: true,
+    zoom: true,
+    actualSize: true,
+    animateThumb: true,
+    zoomFromOrigin: true,
+    speed: 500,
+    licenseKey: 'your_license_key',
+});
