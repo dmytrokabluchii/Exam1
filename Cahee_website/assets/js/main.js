cf0138f39f5546fafbad2d66aef6dcddb0f9b073
Dima <dim84848484@gmail.com>
2022-03-29 15:43:11 +0300

    update chahee_website

---
+++ Cahee_website/assets/js/main.js
@@ -0,0 +1,65 @@
+$(function () {
+    // Fix Header Scroll
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 0) {
+            if (!$("header").hasClass("fixed_header")) {
+                $("header").addClass("fixed_header");
+            }
+        } else {
+            if ($("header").hasClass("fixed_header")) {
+                $("header").removeClass("fixed_header");
+            }
+        }
+    });
+
+    // hamburger-menu
+    $(".hamburger, .page_overlay").on('click', function () {
+        $(".hamburger").toggleClass("is-active");
+        $("body").toggleClass("open");
+    });
+
+    // Плавный Scroll main menu + Скролл по arrow!
+    $(".header__menu ul li a, .virtual-mouse, .header__logo").on('click', function (e) {
+        e.preventDefault();
+        const top = $($(this).attr("href")).offset().top - 80;
+        $('body,html').animate({
+            scrollTop: top + 'px'
+        }, 1100);
+    });
+
+    $('.phone').mask('+38 (099) 999-99-9?9');
+
+    $('.send-form, .subscribe-form').click( function() {
+    	// const form = jQuery(this).closest('form');
+        const form = $(this).closest('form');
+    	if ( form.valid() ) {
+    		form.css('opacity','.5');
+    		let actUrl = form.attr('action');
+
+    		$.ajax({
+    			url: actUrl,
+    			type: 'post',
+    			// dataType: 'html',
+    			data: form.serialize(),
+    			success: function(data) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your message send!',
+                        showConfirmButton: false,
+                        timer: 5000,
+                    });
+    			},
+    			error: function() {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'error',
+                        title: 'Server error, the message not send!',
+                        showConfirmButton: false,
+                        timer: 5000
+                    });
+    			}
+    		});
+    	}
+    });
+});
---
+++ Cahee_website/sender.php
@@ -0,0 +1,21 @@
+<?php
+    $mail = $_POST['subscribe_mail'];
+    $name = $_POST['registration_name'];
+    $email = $_POST['registration_email'];
+	$phone = $_POST['registration_phone'];
+    $text = $_POST['registration_information'];
+
+    $to = "dim8484@ukr.net"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Cahee request";
+	
+	$msg="
+    Subscribe mail: $mail;
+    Registration name: $name;
+    Registration email: $email;
+    Registration phone: $phone;
+    Registration text: $text"; 	
+    mail($to, $subject, $msg, "From: $from ");
+?>
